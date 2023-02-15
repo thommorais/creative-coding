7170f5ed945c46096e783091715fa010c8a2998f
thommorais <thommorais@gmail.com>
2023-02-14 22:58:26 -0300

    clip the rects with a triangule

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,19 @@ import colors from 'riso-colors'
 // Sketch parameters
 const settings = {
 	dimensions: [1080, 1080],
-	animate: true,
 }
 
 function generateValues(qnt: number, width: number, height: number) {
-	const values = [] as { x: number; y: number; w: number; h: number; stroke: string; fill: string }[]
+	const values = [] as {
+		x: number
+		y: number
+		w: number
+		h: number
+		stroke: string
+		fill: string
+		blend: string
+		colors: string[]
+	}[]
 
 	const colorsArr = [random.pick(colors).hex, random.pick(colors).hex, random.pick(colors).hex]
 
@@ -22,7 +30,9 @@ function generateValues(qnt: number, width: number, height: number) {
 		const h = random.range(40, 200)
 		const fill = random.pick(colorsArr)
 		const stroke = random.pick(colorsArr)
-		values.push({ x, y, w, h, fill, stroke })
+		const blend = random.value() > 0.5 ? 'overlay' : 'source-over'
+
+		values.push({ x, y, w, h, fill, stroke, blend, colors: colorsArr })
 	}
 	return values
 }
@@ -31,17 +41,33 @@ function generateValues(qnt: number, width: number, height: number) {
 const sketch = ({ context, width, height }: any) => {
 	const values = generateValues(40, width, height)
 	const bgColor = random.pick(colors).hex
+
+	const mask = {
+		radius: width * 0.4,
+		sides: 3,
+		y: height * 0.58,
+		x: width * 0.5,
+	}
+
 	return ({}: any) => {
 		context.fillStyle = bgColor
 		context.fillRect(0, 0, width, height)
 
-		values.forEach(({ x, y, w, h, stroke, fill }) => {
+		context.save()
+		context.translate(mask.x, mask.y)
+		drawPoligon({ context, radius: mask.radius, sides: mask.sides })
+		context.clip()
+
+		values.forEach(({ x, y, w, h, stroke, fill, blend }) => {
 			context.save()
-			context.strokeStyle = 'blue'
 			context.translate(x, y)
+			context.translate(-mask.x, -mask.y)
 			context.strokeStyle = stroke
 			context.fillStyle = fill
 			context.lineWidth = 10
+
+			context.globalCompositeOperation = blend
+
 			drawSkewedRect({ context, width: w, height: h, degrees: 30 })
 			const shadowColor = color.offsetHSL(fill, 0, 0, -20)
 			shadowColor.rgba[3] = 0.5
@@ -51,8 +77,26 @@ const sketch = ({ context, width, height }: any) => {
 			context.fill()
 			context.shadowColor = null
 			context.stroke()
+
+			context.globalCompositeOperation = 'source-over'
+			context.lineWidth = 2
+			context.strokeStyle = 'black'
+			context.stroke()
+
 			context.restore()
 		})
+
+		context.restore()
+
+		context.save()
+		context.translate(mask.x, mask.y)
+
+		context.globalCompositeOperation = 'color-burn'
+		context.lineWidth = 20
+		context.strokeStyle = values[Math.floor(random.range(1, 39))].colors[0]
+		drawPoligon({ context, radius: mask.radius - context.lineWidth, sides: mask.sides })
+		context.stroke()
+		context.restore()
 	}
 }
 
@@ -76,6 +120,17 @@ const drawSkewedRect = ({ context, width = 600, height = 200, degrees = -45 }: a
 	context.stroke()
 
 	context.restore()
+}
+
+const drawPoligon = ({ context, radius = 100, sides = 3 }: { context: any; radius: number; sides: number }) => {
+	const slice = (Math.PI * 2) / sides
+	context.beginPath()
+	context.moveTo(0, -radius)
+	for (let i = 1; i < sides; i++) {
+		const theta = i * slice - Math.PI * 0.5
+		context.lineTo(Math.cos(theta) * radius, Math.sin(theta) * radius)
+	}
+	context.closePath()
 }
 
 requestIdleCallback(() => {
