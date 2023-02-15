522c8933ce2e3a9e44d2c1bdd9f3a0ef5092e2df
thommorais <thommorais@gmail.com>
2023-02-14 22:23:48 -0300

    add colors and shadows to the skewed rects

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 
 import canvasSketch from 'canvas-sketch'
-import { math, random } from 'canvas-sketch-util'
+import { math, random, color } from 'canvas-sketch-util'
+import colors from 'riso-colors'
 
 // Sketch parameters
 const settings = {
@@ -10,31 +11,45 @@ const settings = {
 }
 
 function generateValues(qnt: number, width: number, height: number) {
-	const values = [] as { x: number; y: number; w: number; h: number }[]
+	const values = [] as { x: number; y: number; w: number; h: number; stroke: string; fill: string }[]
+
+	const colorsArr = [random.pick(colors).hex, random.pick(colors).hex, random.pick(colors).hex]
 
 	for (let i = 0; i < qnt; i++) {
 		const x = random.range(0, width)
 		const y = random.range(0, height)
 		const w = random.range(200, 600)
 		const h = random.range(40, 200)
-		values.push({ x, y, w, h })
+		const fill = random.pick(colorsArr)
+		const stroke = random.pick(colorsArr)
+		values.push({ x, y, w, h, fill, stroke })
 	}
 	return values
 }
 
 // Artwork function
 const sketch = ({ context, width, height }: any) => {
-	const values = generateValues(20, width, height)
+	const values = generateValues(40, width, height)
+	const bgColor = random.pick(colors).hex
 	return ({}: any) => {
-		context.fillStyle = 'white'
+		context.fillStyle = bgColor
 		context.fillRect(0, 0, width, height)
 
-		values.forEach(({ x, y, w, h }) => {
+		values.forEach(({ x, y, w, h, stroke, fill }) => {
 			context.save()
 			context.strokeStyle = 'blue'
 			context.translate(x, y)
-
+			context.strokeStyle = stroke
+			context.fillStyle = fill
+			context.lineWidth = 10
 			drawSkewedRect({ context, width: w, height: h, degrees: 30 })
+			const shadowColor = color.offsetHSL(fill, 0, 0, -20)
+			shadowColor.rgba[3] = 0.5
+			context.shadowColor = color.style(shadowColor.rgba)
+			context.shadowOffsetX = -10
+			context.shadowOffsetY = -10
+			context.fill()
+			context.shadowColor = null
 			context.stroke()
 			context.restore()
 		})
