8965969b8c966981a121ef676139c83b6190b6f2
thommorais <thommorais@gmail.com>
2023-02-15 13:51:25 -0300

    creating random curves

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,14 @@
 import './style.css'
 
 import polyfill from './polyfill'
-
 import canvasSketch from 'canvas-sketch'
-
-import { math, random, color } from 'canvas-sketch-util'
-import colors from 'riso-colors'
+import colorMap from 'colormap'
+import { random, math } from 'canvas-sketch-util'
 
 polyfill()
 // Sketch parameters
 const settings = {
 	dimensions: [1080, 1080],
-}
-
-function generateValues(qnt: number, width: number, height: number) {
-	const values = [] as {
-		x: number
-		y: number
-		w: number
-		h: number
-		stroke: string
-		fill: string
-		blend: string
-		colors: string[]
-	}[]
-
-	const colorsArr = [random.pick(colors).hex, random.pick(colors).hex, random.pick(colors).hex]
-
-	for (let i = 0; i < qnt; i++) {
-		const x = random.range(0, width)
-		const y = random.range(0, height)
-		const w = random.range(200, 600)
-		const h = random.range(40, 200)
-		const fill = random.pick(colorsArr)
-		const stroke = random.pick(colorsArr)
-		const blend = random.value() > 0.5 ? 'overlay' : 'source-over'
-
-		values.push({ x, y, w, h, fill, stroke, blend, colors: colorsArr })
-	}
-	return values
 }
 
 /**
@@ -49,98 +19,88 @@ function generateValues(qnt: number, width: number, height: number) {
  * @property {number} height - height of canvas.
  */
 const sketch = ({ context, width, height }: any) => {
-	const values = generateValues(40, width, height)
-	const bgColor = random.pick(colors).hex
+	const cols = 72
+	const rows = 8
+	const total = cols * rows
 
-	const mask = {
-		radius: width * 0.4,
-		sides: 3,
-		y: height * 0.58,
-		x: width * 0.5,
+	const gridWidth = width * 0.8
+	const gridHeight = height * 0.8
+
+	const cellWidth = gridWidth / cols
+	const cellHeight = gridHeight / rows
+
+	const margins = {
+		x: (width - gridWidth) / 2,
+		y: (height - gridHeight) / 2,
+	}
+
+	const amplitude = 90
+	const frequency = 0.002
+
+	const colors = colorMap({
+		colormap: 'salinity',
+		nshades: amplitude,
+	})
+
+	const points = [] as { x: number; y: number; lineWidth: number; color: string | number[] }[]
+
+	for (let i = 0; i < total; i++) {
+		const x = (i % cols) * cellWidth
+		const y = Math.floor(i / cols) * cellHeight
+
+		const noise = random.noise2D(x, y, frequency, amplitude)
+		const lineWidth = math.mapRange(noise, -amplitude, amplitude, 0, 5)
+
+		const colorIndex = Math.floor(math.mapRange(noise, -amplitude, amplitude, 0, amplitude))
+		const color = colors[colorIndex]
+
+		points.push({ x: x + noise, y: y + noise, lineWidth, color })
 	}
 
 	return (/** @type {SketchFuncProps} */ {}: any) => {
-		context.fillStyle = bgColor
+		context.fillStyle = 'black'
 		context.fillRect(0, 0, width, height)
 
 		context.save()
-		context.translate(mask.x, mask.y)
-		drawPoligon({ context, radius: mask.radius, sides: mask.sides })
-		context.clip()
+		context.translate(margins.x, margins.y)
+		context.translate(cellWidth / 2, cellHeight / 2)
 
-		values.forEach(({ x, y, w, h, stroke, fill, blend }) => {
-			context.save()
-			context.translate(x, y)
-			context.translate(-mask.x, -mask.y)
-			context.strokeStyle = stroke
-			context.fillStyle = fill
-			context.lineWidth = 10
+		// draw lines
+		context.strokeStyle = 'red'
+		context.lineWidth = 4
 
-			context.globalCompositeOperation = blend
+		for (let r = 0; r < rows; r++) {
+			context.beginPath()
 
-			drawSkewedRect({ context, width: w, height: h, degrees: 30 })
-			const shadowColor = color.offsetHSL(fill, 0, 0, -20)
-			shadowColor.rgba[3] = 0.5
-			context.shadowColor = color.style(shadowColor.rgba)
-			context.shadowOffsetX = -10
-			context.shadowOffsetY = -10
-			context.fill()
-			context.shadowColor = null
+			let lastX, lastY
+
+			for (let c = 0; c < cols - 1; c++) {
+				const curr = points[r * cols + c + 0]
+				const next = points[r * cols + c + 1]
+
+				const mx = curr.x + (next.x - curr.x) * 0.8
+				const my = curr.y + (next.y - curr.y) * 5.5
+
+				if (c === 0) {
+					lastX = curr.x
+					lastY = curr.y
+				}
+
+				context.beginPath()
+				context.lineWidth = curr.lineWidth
+				context.strokeStyle = curr.color
+				context.moveTo(lastX, lastY)
+				context.quadraticCurveTo(curr.x, curr.y, mx, my)
+
+				context.stroke()
+				lastX = mx - (c / cols) * 250
+				lastY = my - (r / rows) * 250
+			}
 			context.stroke()
-
-			context.globalCompositeOperation = 'source-over'
-			context.lineWidth = 2
-			context.strokeStyle = 'black'
-			context.stroke()
-
-			context.restore()
-		})
+		}
 
 		context.restore()
-
-		context.save()
-		context.translate(mask.x, mask.y)
-
-		context.globalCompositeOperation = 'color-burn'
-		context.lineWidth = 20
-		context.strokeStyle = values[Math.floor(random.range(1, 39))].colors[0]
-		drawPoligon({ context, radius: mask.radius - context.lineWidth, sides: mask.sides })
-		context.stroke()
-		context.restore()
 	}
-}
-
-const drawSkewedRect = ({ context, width = 600, height = 200, degrees = -45 }: any) => {
-	const angle = math.degToRad(degrees)
-
-	const rx = Math.cos(angle) * width
-	const ry = Math.sin(angle) * width
-
-	context.save()
-	context.translate(rx * -0.5, (ry + height) * -0.5)
-
-	context.strokeStyle = 'blue'
-
-	context.beginPath()
-	context.moveTo(0, 0)
-	context.lineTo(rx, ry)
-	context.lineTo(rx, ry + height)
-	context.lineTo(0, height)
-	context.closePath()
-	context.stroke()
-
-	context.restore()
-}
-
-const drawPoligon = ({ context, radius = 100, sides = 3 }: { context: any; radius: number; sides: number }) => {
-	const slice = (Math.PI * 2) / sides
-	context.beginPath()
-	context.moveTo(0, -radius)
-	for (let i = 1; i < sides; i++) {
-		const theta = i * slice - Math.PI * 0.5
-		context.lineTo(Math.cos(theta) * radius, Math.sin(theta) * radius)
-	}
-	context.closePath()
 }
 
 requestIdleCallback(() => {
